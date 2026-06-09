7cbfbc286d4960dd5effd6b38323446b352618ae
mosesogheneo <mosesogheneo@gmail.com>
2026-06-10 00:22:45 +0100

    Configured api documentation

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
 import uuid
 from datetime import datetime, timezone
 
+from flasgger import Swagger
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/docs"
+}
+swagger = Swagger(app, config=swagger_config)
+
 @app.route('/api/health', methods=['GET'])
 def health():
+    """
+        Return server status
+        ---
+        responses:
+          200:
+            description: Returns the health status of the NTTS order API
+            schema:
+              type: object
+              properties:
+                status:
+                  type: string
+                  example: ok
+                timestamp:
+                  type: string
+                  example: 2026-06-10T12:00:00Z
+                service:
+                  type: string
+                  example: ntts-order-api
+        """
     return jsonify({
         "status": "ok",
         "timestamp": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
@@ -16,6 +52,38 @@ def health():
 
 @app.route('/api/orders', methods=['POST'])
 def create_order():
+    """
+        Accept order, return receipt
+        ---
+        parameters:
+          - in: body
+            name: body
+            required: true
+            description: JSON payload containing product details
+            schema:
+              type: object
+              required:
+                - product_name
+                - quantity
+                - unit_price
+              properties:
+                product_name:
+                  type: string
+                  example: Spicy Chicken Pizza
+                quantity:
+                  type: integer
+                  example: 2
+                unit_price:
+                  type: number
+                  format: float
+                  example: 4500.00
+        responses:
+          200:
+            description: Successfully calculated order receipt
+          400:
+            description: Bad input (missing or invalid fields)
+        """
+
     data = request.get_json()
 
     if not data or not all(k in data for k in ("product_name", "quantity", "unit_price")):
