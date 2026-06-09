e18b1d4160b3f3aa50f7aad548aa9ef24698fd87
mosesogheneo <mosesogheneo@gmail.com>
2026-06-10 00:13:48 +0100

    created the /order api route

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import uuid
 from datetime import datetime, timezone
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -12,3 +13,37 @@ def health():
         "service": "ntts-order-api"
     }), 200
 
+
+@app.route('/api/orders', methods=['POST'])
+def create_order():
+    data = request.get_json()
+
+    if not data or not all(k in data for k in ("product_name", "quantity", "unit_price")):
+        return jsonify({"error": "Bad Request: Missing required fields"}), 400
+
+    try:
+        qty = int(data['quantity'])
+        price = float(data['unit_price'])
+    except ValueError:
+        return jsonify({"error": "Bad Request: Invalid data types for quantity or unit_price"}), 400
+
+
+    total = qty * price
+
+
+    receipt = {
+        "status": "success",
+        "receipt": {
+            "order_id": str(uuid.uuid4()),
+            "product_name": data['product_name'],
+            "quantity": qty,
+            "unit_price": price,
+            "total": total,
+            "currency": "NGN"
+        }
+    }
+
+    return jsonify(receipt), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
